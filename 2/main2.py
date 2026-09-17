9450ac442f44645dd97744810d0e24b00473bfb6
SOQFAQL <snapdrag238@gmail.com>
2026-09-17 23:02:31 +0300

    main2.py : added plag matrix output func, fixed some tabs and spaces

--- 2/main2.py
+++ 2/main2.py
@@ -2,15 +2,16 @@ import math
 import array
 
 #варіант 15
-# -7x1 + 3x2 + 2x3 = 13
-#          
-#
+# -7x1 +3x2 +2x3 = 13
+# -3x1 -7x2 -2x3 = 25       
+# -x1 -2x2 -6x3 = 13
 
 class Slar :
     def __init__(self, nInp: int) :
         self.n = nInp
         self.matrixA = [[0] * self.n in range(self.n)]
         self.matrixB = [self.n]
+        self.matrixAns = [[0] * self.n in range(self.n)]
 
     def fillMatrix(inpType : str) -> bool:
         #print("Виберіть як вихочети заповнити СЛАР" \
@@ -31,7 +32,7 @@ class Slar :
             case "cmd":
                 for i in range(Slar.n) :
                     for j in range(Slar.n) :
-                        print(f"Введіть префіікс невідомого x{i}{j} >> ",end="")
+                        print(f"Введіть префіксну частину невідомого x{i}{j} >> ",end="")
                         Slar.matrixA[i][j] = float[input()]
                 for i in range(Slar.n) :
                     print(f"Введіть вільний член b{i} >> ", end="")
@@ -51,10 +52,13 @@ class Slar :
     def methodZeidel(self) -> bool:
         pass
 
-
-
-    def main() :
+    def finalMatrixOutput(self) -> bool :
         pass
 
-    if __name__ == "__main2__" :
-        main()  
+
+
+def main() :
+    pass
+
+if __name__ == "__main2__" :
+    main()  
